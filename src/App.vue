498ccc34041181f12c7d668fa6663bd8fc44e347
mt <1344061657@qq.com>
2023-06-08 23:55:03 +0800

    创建项目

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        <router-view/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data() {
+        return {
+            resizeFn: null
+        }
+    },
+    mounted() {
+        const documentWidth = document.body.offsetWidth;
+        const ratio = documentWidth / 1920;
+        if (documentWidth > 1920) {
+            document.body.style.transform = `scale(${ratio}, ${ratio})`
+        }
+        this.resizeFn = this.$debounce(function () {
+            const documentWidth = document.body.offsetWidth;
+            const ratio = documentWidth / 1920;
+            if (documentWidth > 1920) {
+                document.body.style.transform = `scale(${ratio}, ${ratio})`
+            }
+        }, 200)
+        window.addEventListener('resize',this.resizeFn);
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.resizeFn);
+    }
 }
 </script>
 
-<style>
+<style lang="less">
+html,body {
+    height: 100%;
+    width: 100%;
+    padding:0;
+    margin:0;
+}
+html {
+    font-size: 20px;
+}
+body {
+    transform-origin: left top;
+    background-size: 100% 100%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    height: 100%;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
 </style>
